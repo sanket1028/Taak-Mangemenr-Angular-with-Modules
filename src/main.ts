b218af0dc4d0b4344e10de07e2e93294b9a2c52d
sanket1028 <sanket.bulkunde7@gmail.com>
2024-06-13 20:58:58 +0530

    Implementation using modules is added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+/* import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
+// this approach works only in standalone component
+*/
+
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+platformBrowserDynamic().bootstrapModule(AppModule);
